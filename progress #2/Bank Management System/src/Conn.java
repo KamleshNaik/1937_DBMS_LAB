5981cdd30f9ba08a733af59921158aedf5b26293
KamleshNaik <mca.1937@unigoa.ac.in>
2020-11-26 13:50:55 +0530

    progress #2

---
+++ progress #2/Bank Management System/src/Conn.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+
+public class Conn {
+	Connection c;
+	Statement s;
+	public Conn() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			c =DriverManager.getConnection("jdbc:mysql:///DBMS_Project");
+			s =c.createStatement();
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}
